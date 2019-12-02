6ac6f049f314f527a765f06e536ddde7b972653a
jumbojet33 <54554590+jumbojet33@users.noreply.github.com>
2019-12-02 17:07:28 -0300

    Update lezcodeprojeto.py

--- lezcodeprojeto.py
+++ lezcodeprojeto.py
@@ -24,3 +24,48 @@ print(f"open                     =  {opn.text}")
 #dontpad (link=sublimelc)
 #adicionar os mesmos critérios do S&P por setor, adicionar lista de empresas separadas por setor, comparar a empresa em questão
 #com resto do S&P e lista.
+
+
+#defining o portifolio
+
+Perfil1 = input("In a scale from 1-10, how would you say you deal with risk? One being I am literally scared of everything and 10 means you eat fear")
+
+Index = ("S&P500, NASDAQ, DOW30, RUSSEL2000")
+
+if Perfil1 > "6":
+	print("Risk it is")
+else:
+	print("Lets take is slow")
+
+Perfil2 = input("Do you have any preference in market/index?(Y/N)")
+if Perfil2 == "Y":
+	input("Select One:")
+else:
+	print("Lets go")
+
+
+if beta >"1":
+	print("High Risk High Reward")
+else:
+	print("Slow and Steady")
+
+if PE > "25":
+	print("Favouralbe Ratio")
+else:
+	print("Lets look for something else...")
+
+if div > "1.8":
+	print("good dividend yield")
+else:
+	print("would be intresting to look for something else")
+
+
+
+
+
+
+HIGHRISK = (High Beta, Low PE, Average - Low Divdend)
+LOWRISKS = (Low Beta, High PE, High Dividend)
+
+
+
